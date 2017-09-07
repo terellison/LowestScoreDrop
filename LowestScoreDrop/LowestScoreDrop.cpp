4d4b4ca67489b3c5a45cccad08c4cd8ebf89f090
Terry Ellison <ellisonterry4@gmail.com>
2017-09-07 15:25:24 -0400

    Add project files.

---
+++ LowestScoreDrop/LowestScoreDrop.cpp
@@ -0,0 +1,22 @@
+
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void getScore(double &score);
+
+int main()
+{
+	
+    return 0;
+}
+
+void getScore(double &score) {
+	double &score = 1;
+	cout << "Enter a test score: ";
+	cin >> score;
+
+}
+
